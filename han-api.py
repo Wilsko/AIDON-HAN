66b121c55eb173c60332fb630c83c99e1a36a965
Ville Piispa <ville.piispa@gmail.com>
2025-05-29 18:07:26 +0000

    added use_reloader=False to avoid multiple processes

--- han-api.py
+++ han-api.py
@@ -130,4 +130,4 @@ def get_monitor_file():
         return jsonify({"status": "error", "message": f"File '{MONITOR_FILE}' not found."})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5002)
+    app.run(debug=True, host='0.0.0.0', port=5002, use_reloader=False)
